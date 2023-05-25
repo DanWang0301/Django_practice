345da353b0db4550cc1d5fda6df55e29300a185a
DanWang0301 <dan08080301@gmail.com>
2023-05-26 01:05:42 +0800

    Lesson 2:Create models anf database

--- django_practice/practice/admin.py
+++ django_practice/practice/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from practice.models import Visitor
+
+admin.site.register(Visitor)
--- django_practice/practice/models.py
+++ django_practice/practice/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Visitor(models.Model):
+    Name = models.TextField("")
+    title = models.TextField(default="guest")
+    last_come_date = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "visitor"
